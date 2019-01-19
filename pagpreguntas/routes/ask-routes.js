f5b79712c40c318c9dd9ca86bd3d795eb19bea28
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2019-01-19 15:33:18 -0600

    Homologado V1

---
+++ pagpreguntas/routes/ask-routes.js
@@ -0,0 +1,175 @@
+const express = require("express");
+const askRoutes = express.Router();
+const zxcvbn = require("zxcvbn");
+const passport = require("passport");
+const ensureLogin = require("connect-ensure-login");
+const User = require("../models/User");
+const Pregunta = require("../models/Pregunta");
+const Respuesta = require("../models/Respuesta");
+const Interes = require("../models/Interes");
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+askRoutes.get("/signup",(req,res,next)=>{
+  res.render("auth/signup");
+});
+
+askRoutes.post("/signup",(req,res,next)=>{
+  const username=req.body.username;
+  const password=req.body.password;
+
+  if(username==="" || password===""){
+    res.render("auth/signup",{
+      message: "Indica un nombre de usuario y contraseña"
+    })
+    return;
+  }
+
+  if(zxcvbn(password).score < 1){
+    res.render("auth/signup",{
+      message: "El nivel de complejidad de tu password es bajo, favor de ingresar numeros, letras y caracteres especiales"
+    })
+    return;
+  }
+
+  User.findOne({username})
+  .then(user=>{
+    if(user !== null){
+      res.render("auth/signup",{
+        message: "El usuario ingresado ya existe"
+      })
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password,salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    })
+
+    newUser.save((err)=>{
+      if(err){
+        res.render("auth/signup",{
+          message: "No fue posible guardar el registro. Intetalo mas tarde"
+        })
+      }else{
+        res.redirect("/login");
+      }
+    })
+  })
+  .catch(error=>{
+    next(error)
+  })
+})
+
+askRoutes.get("/login",(req,res,next)=>{
+  res.render("auth/login", {message:req.flash("error")})
+})
+
+askRoutes.post("/login", passport.authenticate("local",{
+  successRedirect:"/private-page",
+  failureRedirect:"/login",
+  failureFlash:true,
+  passReqToCallback:true
+}))
+
+askRoutes.get("/userpage", ensureLogin.ensureLoggedIn(),(req,res,next)=>{
+  res.render("user-page",{user:req.user})
+})
+
+/////////////////////////////////Slack login////////////////////
+askRoutes.get("/auth/slack", passport.authenticate("slack"));
+askRoutes.get("/auth/slack/callback", passport.authenticate("slack", {
+  successRedirect: "/userpage",
+  failureRedirect: "/"
+}));
+
+////////////////////Consultas sin login//////////////////////////
+router.get('/preguntas', (req, res)=>{
+  Pregunta.find()
+    .populate("tag", "respuestas")
+    .then(preguntas =>{
+      res.render('preguntas-all', {preguntas})
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+})
+
+router.get('/preguntas/:cat', (req, res)=>{
+  const cat = req.params.cat;
+  Pregunta.find({tag : cat})
+    .populate("tag", "respuestas")
+    .then(preguntas =>{
+      res.render('preguntas-all', {preguntas})
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+})
+
+
+////////////////////CRUD preguntas login/////////////////////////
+//Consultas
+router.get('/preguntas/:id/user', (req, res)=>{
+  User.find()
+    .populate("preguntas","respuestas","tags")
+    .then(user =>{
+      res.render('preguntas-all', {user})
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+})
+
+router.get('/preguntas/:cat/consulta/:id/user', (req, res)=>{
+  const cat = req.params.cat;
+  const id = req.params.id;
+  User.find($and[{'_id': id},{"tag": cat}])
+    .populate("tag", "respuestas")
+    .then(user =>{
+      res.render('preguntas-all', {user})
+    })
+    .catch(err =>{
+      console.log(err)
+    })
+})
+
+//Altas
+router.get("/preguntas/:id/add",(req,res)=>{
+  res.render("pregunta-nueva")
+})
+
+router.post("/preguntas/add",(req,res)=>{
+  const {pregunta, tag} = req.body;
+  const newPregunta = new Pregunta({pregunta,tag});
+  newPregunta.save()
+  .then((pregunta)=>{
+    res.redirect(301,"/preguntas")
+  })
+  .catch(err=>console.log(err));
+})
+
+router.post('/preguntas/:id/delete', (req, res)=>{
+  let preguntaId = req.params.id
+  console.log(preguntaId);
+  Pregunta.findByIdAndRemove({'_id': preguntaId})
+  .then((pregunta)=>{
+    res.render('/preguntas')
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
+})
+
+
+
+askRoutes.get("/logout",(req,res,next)=>{
+  req.logout();
+  res.redirect("login");
+})
+
+module.exports= askRoutes;
--- pagpreguntas/routes/auth-routes.js
+++ pagpreguntas/routes/auth-routes.js
@@ -1,70 +1,75 @@
-const express = require ('express')
-const authRoutes = express.Router ()
+const express = require('express')
+const authRoutes = express.Router()
 
-const User = require ('../models/user')
+const User = require('../models/User')
 
 const bcrypt = require('bcrypt')
 const bcryptSalt = 10
-const zxcvbn = require ('zxcvbn')
-const passport = require ('passport')
-const ensureLogin = require ('connect-ensure-login')
+const zxcvbn = require('zxcvbn')
+const passport = require('passport')
+const ensureLogin = require('connect-ensure-login')
 
 
 
-authRoutes.get ('/signup', (req, res, next)=>{
+authRoutes.get('/signup', (req, res, next) => {
   res.render('auth/signup')
 
 })
 
-authRoutes.post ('/signup', (req, res, next)=>{
-  const username = req.body.username
-  const password = req.body.password
+authRoutes.post('/signup', (req, res, next) => {
+  const {username, password, email} = req.body;
 
-  if(username == "" || password == ""){
-    res.render ('/auth/signup',{
-      message:'Indica un nombre de usuario y contraseña'
+  if (username == "" || password == "") {
+    res.render('/auth/signup', {
+      message: 'Indica un nombre de usuario y contraseña'
     })
-    return
+    return;
   }
-  if (zxcvbn(password).score < 1){
-    res.render('auth/signup',{
-      message:zxcvbn (password).feedback.warning
+  if (zxcvbn(password).score < 1) {
+    res.render('auth/signup', {
+      message: zxcvbn(password).feedback.warning
     })
+    return;
   }
-  User.findOne ({username})
-  .then (user =>{
-    if (user !== null){
-      res.render ('auth/signup',{
-        message:'El usuario ingresado YA existe'
-      })
-      return
-    }
-    const salt = bcrypt.genSaltSync ( bcryptSalt)
-    const hashPass = bcrypt.hashSync(password, salt)
-
-    const newUser = new User({
-      username,
-      password: hashPass
-    })
-
-    newUser.save((err)=>{
-      if (err) {
-        res.render ('auth/signup', {
-          message:'Algo salió mal y no puedo guardar tu registro. Inténtalo de nuevo más tarde'
+  User.findOne({ username })
+    .then(user => {
+      if (user !== null) {
+        res.render('auth/signup', {
+          message: 'El usuario ingresado YA existe'
         })
-      } else {
-        res.redirect ('/')
-         }
-      })
-    })
-    .catch(error =>{
-      next(error)
-    })
-  })
+        return;
+      }
+      const salt = bcrypt.genSaltSync(bcryptSalt)
+      const hashPass = bcrypt.hashSync(password, salt)
 
-authRoutes.get ('/auth/google', passport.authenticate("google",{
+      const newUser = new User({
+        username,
+        password: hashPass,
+        email : email,
+        preguntas : [],
+        respuestas : [],
+        tags : [],
+        googleID : "None yet."
+      })
+
+      newUser.save()
+        .then(() => {
+          res.redirect("/");
+          console.log("Se pudo");
+        })
+        .catch(error => {
+          next(error)
+        })
+    })
+    .catch(err => {
+      console.log(err)
+      next();
+    });
+});
+
+authRoutes.get('/auth/google', passport.authenticate("google", {
   scope: ["https://www.googleapis.com/auth/plus.login",
-        "https://www.googleapis.com/auth/plus.profile.emails.read"]
+    "https://www.googleapis.com/auth/plus.profile.emails.read"]
 }));
 
 authRoutes.get('/auth/google/callback', passport.authenticate("google", {
@@ -74,29 +79,29 @@ authRoutes.get('/auth/google/callback', passport.authenticate("google", {
 
 
 authRoutes.get('/auth/slack', passport.authenticate("slack"));
-authRoutes.get('/auth/slack/callback', passport.authenticate("slack",{
-  successRedirect:"/private-page",
-  failureRedirect:"/"
+authRoutes.get('/auth/slack/callback', passport.authenticate("slack", {
+  successRedirect: "/private-page",
+  failureRedirect: "/"
 }));
 
-authRoutes.get('/login', (req, res, next)=>{
-  res.render ('auth/login',{message: req.flash("error")})
+authRoutes.get('/login', (req, res, next) => {
+  res.render('auth/login', { message: req.flash("error") })
 })
 
-authRoutes.post('/login', passport.authenticate("local",{
+authRoutes.post('/login', passport.authenticate("local", {
   successRedirect: "/private-page",
-  failureRedirect : "/login",
+  failureRedirect: "/login",
   failureFlash: true,
-  passReqToCallback:true
+  passReqToCallback: true
 }))
 
-authRoutes.get ('/private-page', ensureLogin.ensureLoggedIn(),(req, res, next)=>{
-  res.render ('private', { user: req.usehr})
+authRoutes.get('/private-page', ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  res.render('private', { user: req.usehr })
 })
 
-authRoutes.get ('/logout', (req, res, next)=>{
+authRoutes.get('/logout', (req, res, next) => {
   req.logout()
   res.redirect('login')
 })
 
-module.exports = authRoutes
+module.exports = authRoutes;
